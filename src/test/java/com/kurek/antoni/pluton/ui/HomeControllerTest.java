1d31994ed78d6e0dc137a3dee951d72c1f966746
Antoni Kurek <antoni.kurek@gmail.com>
2022-03-05 23:17:27 +0100

    Mock security in test

--- src/test/java/com/kurek/antoni/pluton/ui/HomeControllerTest.java
+++ src/test/java/com/kurek/antoni/pluton/ui/HomeControllerTest.java
@@ -1,13 +1,14 @@
 package com.kurek.antoni.pluton.ui;
 
+import com.kurek.antoni.pluton.ui.config.TestUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -17,18 +18,20 @@ class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-
     @Test
-    @WithUserDetails
-        //todo mock user details
     void shouldPullListOfOwnedPortfolios() throws Exception {
         //given authenticated user
+        String userName = "Jan Sarna";
+        TestUser user = new TestUser(userName);
         //when home page is viewed
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get("/")
+                                .with(oauth2Login()
+                                              .oauth2User(user)))
                .andExpect(status().isOk())
                .andExpect(view().name("home"))
-               .andExpect(content().string(containsString("Welcome to...")));
+               .andExpect(content().string(containsString("Welcome " + userName)));
         //then portfolio-storage api should be called
+        //todo make api call
     }
 
 }
---
+++ src/test/java/com/kurek/antoni/pluton/ui/config/TestUser.java
@@ -0,0 +1,39 @@
+package com.kurek.antoni.pluton.ui.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@AllArgsConstructor
+public class TestUser implements OAuth2User {
+    private final String email;
+    private final String name;
+
+    public TestUser(String name) {
+        this.email = "user@domain.host";
+        this.name = name;
+    }
+
+    public TestUser() {
+        this.email = "user@domain.host";
+        this.name = "John Doe";
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of("email", email, "name", name);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "what is this used for?";
+    }
+}
